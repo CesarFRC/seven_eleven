ed56bca3f1c7077d21c431eb11068fe66e779a44
donlitle <cesar.reyes.cortinas12@gmail.com>
2025-11-17 14:11:25 -0600

    Agregado boton para salir fuera del juego

---
+++ seven_eleven/JuegoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  JuegoViewController.swift
+//  seven_eleven
+//
+//  Created by donlike on 16/11/25.
+//
+
+import UIKit
+
+class JuegoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
