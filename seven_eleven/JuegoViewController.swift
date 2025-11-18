2606dbeee855f21a923c0297edf96b09594309eb
donlitle <cesar.reyes.cortinas12@gmail.com>
2025-11-18 17:11:02 -0600

    ahora si quedo bien el menu del juego en todos los dispositivos

--- seven_eleven/JuegoViewController.swift
+++ seven_eleven/JuegoViewController.swift
@@ -1,9 +1,4 @@
-//
-//  JuegoViewController.swift
-//  seven_eleven
-//
-//  Created by donlike on 16/11/25.
-//
+
 
 import UIKit
 
@@ -12,18 +7,54 @@ class JuegoViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var dadoIzquierdo: UIImageView!
+    
+    @IBOutlet weak var dadoDerecho:
+    UIImageView!
+    @IBAction func accionBotonAtras(_ remitente: UIButton) {
+        cerrarVistaModal()
     }
-    */
+    
+    func cerrarVistaModal() {
+        self.dismiss(animated: true) {
+           
+            
+        }
+    }
+    
+    func animardados(){
+        let imagenesdadoizquierda = [
+            UIImage(named: "dado1")!,
+            UIImage(named: "dado2")!,
+            UIImage(named: "dado3")!,
+            UIImage(named: "dado4")!,
+            UIImage(named: "dado5")!,
+            UIImage(named: "dado6")!
+        ]
+        
+        dadoIzquierdo.animationImages = imagenesdadoizquierda
+        dadoIzquierdo.animationDuration = 0.5
+        dadoIzquierdo.animationRepeatCount = 1
+        dadoIzquierdo.startAnimating()
+        
+        let resultadoIzquierdo = Int.random(in: 1...6)
+        let nombreImagenFinal = "Dado1_resultado_\(resultadoIzquierdo)"
+        
+        let tiempoAnimacion: TimeInterval = 0.5
+        
+        //DispatchQueue.main.asyncAfter(deadline: .now() + tiempoAnimacion){
+            //self.dadoIzquierdo.stopAnimating()
+            //self.dadoIzquierdo.image = UIImage(named: nombreImagenFinal)
+        //}
+        
+    }
 
+    @IBAction func Tirar(_ sender: Any) {
+        
+        animardados()
+    }
 }
